c803d5680228b12052d3241d8ebe369c40c07fbf
JuliaGrisales <jgrisales@gmail.com>
2020-09-03 17:54:11 +0200

    Creating tables with php artisan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Migrations
+                Migrations 😎
             </div>
 
             <div class="links">
